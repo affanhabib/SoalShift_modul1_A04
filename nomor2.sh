99351fd17a8d569dc4933793d6ccf2e39a45873d
Affan Ahsanul Habib <ahsanulhabib11@gmail.com>
2019-02-22 17:29:35 +0700

    Update
    Update readme + upload jawaban

---
+++ nomor2.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+echo "A. Negara dengan Penjualan Terbanyak:"
+awk -F ',' '{if($7=="2012") qnt[$1]+=$10;} END {for(i in qnt) print i}' WA_Sales_Products_2012-14.csv | sort -n -r | head -1 
+
+echo "B. 3 Product Line Teratas pada Negara dengan Penjualan Terbanyak:"
+awk -F ',' '{if($7=="2012" && $1=="United States") qnt[$4]+=$10;} END{for(i in qnt) print qnt[i] "-" i}' WA_Sales_Products_2012-14.csv | sort -n -r | head -3 | awk -F '-' '{print "- " $2}'
+
+echo "C. 3 Product Teratas pada Product Line Teratas:"
+awk -F ',' '{if($7=="2012" && $1=="United States" &&($4=="Personal Accessories" || $4=="Outdoor Protection" || $4=="Camping Equipment")) qnt[$6]+=$10;} END {for(i in qnt) print qnt[i] "-" i}' WA_Sales_Products_2012-14.csv | sort -n -r | head -3 | awk -F '-' '{print "* " $2}'
